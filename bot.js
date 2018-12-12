3ece6f7801526e52201e2c6164513b44b01392e9
kmindspark <19kaushiks@gmail.com>
2018-12-12 13:10:09 -0800

    Basic testing

--- bot.js
+++ bot.js
@@ -4,14 +4,32 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
+var userList = [];
+var userDaily = [];
+var userTotal = [];
 
+function getUsers() {
+   const list = client.guilds.get("436232448121241622");
+
+   // Iterate through the collection of GuildMembers from the Guild getting the username property of each member 
+   for (i = 0; i < list.length; i++) {
+      userList.push(list[i].id);
+   }
+}
+
+client.on('ready', () => {
+   getUsers();
 });
 
 client.on('message', message => {
    if (message.content === "m ping") {
-      message.reply("Hello.");
+      message.reply("Pong.");
+
+      for (i = 0; i < userList.length; i++) {
+         message.reply(userList[i]);
+      }
    }
+
 });
 
 // THIS  MUST  BE  THIS  WAY
