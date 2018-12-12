14af8bf78b179b089d1d6aa90908ffb669df866a
kmindspark <19kaushiks@gmail.com>
2018-12-12 14:25:55 -0800

    more testing

--- bot.js
+++ bot.js
@@ -13,10 +13,10 @@ function getUsers() {
    var curGuildMembers = client.guilds.get("436232448121241622").members;
 
    // Iterate through the collection of GuildMembers from the Guild getting the username property of each member 
-   for (i = 0; i < curGuildMembers.length; i++) {
+   /*for (i = 0; i < curGuildMembers.length; i++) {
       //userList.push(list[i].id);
       //count++;
-   }
+   }*/
 
    count = 1;
 }
